ab63c80d148d8f7900cb0d4956d3df2cfea841d7
marvin <marvin23m@gmail.com>
2020-09-28 13:31:36 +0100

    Make functional search form to get stock info

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -5,15 +5,19 @@ def home(request):
     import requests
     import json
 
-    # pk_40b606f9a69843238ff515cf84143173
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_40b606f9a69843238ff515cf84143173")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token=pk_40b606f9a69843238ff515cf84143173")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-       api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+           api = "Error..."
 
-    return render(request, 'home.html', {'api': api})
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker symbol above..."})
+
 
 def about(request):
     return render(request, 'about.html', {})
