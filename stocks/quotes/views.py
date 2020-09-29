ee508c48cb1ea7348d330fc998595fbd667d7de6
marvin <marvin23m@gmail.com>
2020-09-29 09:51:50 +0100

    Add ability to delete and django messages

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 def home(request):
     import requests
@@ -23,4 +25,20 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
-    return render(request, 'add_stock.html', {})
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added"))
+            return redirect('add_stock')
+
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted!"))
+    return redirect(add_stock)
